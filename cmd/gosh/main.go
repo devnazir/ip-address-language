b5c6c7d1aca97c19dcfd0bbe990c6c150711eb74
devnazir <nazirbachrululum@gmail.com>
2024-11-08 19:55:41 +0700

    chore: delete test code

--- cmd/gosh/main.go
+++ cmd/gosh/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"regexp"
 
 	"github.com/devnazir/gosh-script/internal/interpreter"
 	"github.com/devnazir/gosh-script/internal/lexer"
@@ -12,32 +11,6 @@ import (
 )
 
 func main() {
-	text := "Hello World Gdas $hello1$hello2$hello3 $hello4 asdasdas"
-
-	// Match all text that does not start with `$`
-	nonVarRe := regexp.MustCompile(`(?:^|[^$])\b([A-Za-z\s]+)\b(?:[^$]|$)`)
-	// Match all variables that start with `$`
-	varRe := regexp.MustCompile(`\$\w+`)
-
-	// Find all non-variable parts
-	nonVarMatches := nonVarRe.FindAllString(text, -1)
-	// Join non-variable parts into a single string
-	nonVarText := ""
-	for _, match := range nonVarMatches {
-		nonVarText += match
-	}
-
-	// Find all variable parts
-	varMatches := varRe.FindAllString(text, -1)
-	// Join variable parts into a single string
-	varText := ""
-	for _, match := range varMatches {
-		varText += match + " "
-	}
-
-	// Print the results
-	fmt.Println("String is:", nonVarText)
-	fmt.Println("Vars are:", varText)
 
 	if len(os.Args) < 2 {
 		recovery := func() {
